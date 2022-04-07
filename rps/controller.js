d4155ac07f91f7e891e6c9c37bdbc9236dd108c9
smamer <smamer@milestechnologies.com>
2022-04-07 17:53:36 -0500

    final

--- rps/controller.js
+++ rps/controller.js
@@ -7,12 +7,12 @@ function playGame() {
     // Call the function game.newGame() to reset the counts.
     let numberOfRounds = prompt("Please enter how many rounds you would like to play");
     numberOfRounds = parseInt(numberOfRounds, 10)
-    if (game.IsNumberOfRoundsValid(numberOfRounds)) {
+    if (game.isNumberOfRoundsValid(numberOfRounds)) {
         game.setNumberOfRounds(numberOfRounds);
     } else {
         numberOfRounds = prompt("Invalid entry. You must enter a value between 1 and 5");
         numberOfRounds = parseInt(numberOfRounds, 10)
-        if (game.IsNumberOfRoundsValid(numberOfRounds)) {
+        if (game.isNumberOfRoundsValid(numberOfRounds)) {
             game.setNumberOfRounds(numberOfRounds);
         }
     }
@@ -22,11 +22,24 @@ function playGame() {
     //  >> Else, try to get the number of rounds for the suer again
 
 
-    playRound(numberOfRounds)
+    for(var i = 1; i <= numberOfRounds; i++) {
+        playRound(i);
+        var currentRound = numberOfRounds;
+        currentRound =- 1;
+    }
     // Call playRound() with a parameter of the round count (1 thru numberOfRounds) for each round 
 
 
-    console.log()
+    console.log("Rounds played: " + game.numberOfRounds + " Ties: " + game.countOfTies);
+    console.log(`Rounds played: ${game.numberOfRounds} Ties: ${game.countOfTies}`)
+    console.log(`Player Final Score: ${game.countOfPlayerWins} Computer Final Score: ${game.countOfComputerWins}`);
+    if(game.countOfPlayerWins > game.countOfComputerWins) {
+        console.log("Player Wins!");
+    } else if (game.countOfComputerWins > game.countOfPlayerWins) {
+        console.log("ComputerWins!");
+    } else {
+        console.log("Tie");
+    }
     // Display the Final Score from the properties stored in the Game object
 
 }
@@ -36,7 +49,8 @@ function playGame() {
  * @param {*} roundNumber
  */
 function playRound(roundNumber) {
-    //*********************************ADD CODE HERE *************************************/ console.log()
+    console.log(`Current Round: ${roundNumber}`)
+    console.log(`Player Score: ${game.countOfPlayerWins} Computer Score: ${game.countOfComputerWins} Ties ${game.countOfTies}`)
     // Display the Current Score to the User
     
     //Prompt for Player Selection
@@ -46,7 +60,20 @@ function playRound(roundNumber) {
     //Play the Round
     var outcome = round.determineWinner();
 
-    //*********************************ADD CODE HERE *************************************/ console.log()
+    switch (outcome)
+    {
+        case "player":
+            game.incrementPlayerWins();
+            break;
+        case "computer":
+            game.incrementComputerWins();
+            break;
+        case "tie":
+            game.incrementCountOfTies();
+            break;
+    }
+    console.log(`Player Chose: ${playerSelection} Computer Chose: ${round.computerSelection}`);
+    console.log(`Winner is: ${outcome}`);
     // Display the Round Results from the properties stored in the Round object
 
 }
@@ -59,7 +86,20 @@ function playRound(roundNumber) {
  */
 function getPlayerSelection() {
     
-    playerSelection = prompt("Enter your move (must be 'ROCK', 'SCISSORS', or 'PAPER'")
+    let playerSelection = prompt("Enter your move (must be 'rock', 'scissors', or 'paper')");
+    switch (playerSelection) {
+        case "rock":
+            return Selections.ROCK;
+            break;
+        case "scissors":
+            return Selections.SCISSORS;
+            break;
+        case "paper":
+            return Selections.PAPER;
+            break;
+        default:
+            return Selections.ROCK;
+    }
     // Prompt, Validate and Return the player selection. This must be in the form of "rock", "paper", or "scissors" exactly
 }
 
--- rps/round.js
+++ rps/round.js
@@ -52,37 +52,38 @@ class Round{
      * @returns Outcomes
      */
     determineWinner() {
-        switch (this.playerSelection === Selections.ROCK) {
-            case this.computerSelection === Selections.PAPER:
-                game.incrementComputerWins();
-                return Outcomes.COMPUTER_WINS;
+        switch (this.playerSelection) {
+            case "scissors":
+                if (this.computerSelection === "rock")
+                {
+                    return Outcomes.COMPUTER_WINS;
+                } else if (this.computerSelection === "scissors") {
+                    return Outcomes.TIE;
+                } else {
+                    return Outcomes.PLAYER_WINS;
+                }
                 break;
-            case this.computerSelection = ROCK:
-                text = TIE;
+            case "rock":
+                if (this.computerSelection === "paper")
+                 {
+                    return Outcomes.COMPUTER_WINS;
+                } else if (this.computerSelection === "rock") {
+                    return Outcomes.TIE;
+                } else {
+                    return Outcomes.PLAYER_WINS;
+                }
+                 break;
+            case "paper":
+                if (this.computerSelection === "scissors")
+                {
+                    return Outcomes.COMPUTER_WINS;
+                } else if (this.computerSelection === "paper") {
+                    return Outcomes.TIE;
+                } else {
+                    return Outcomes.PLAYER_WINS;
+                }
                 break;
-            case this.computerSelection = SCISSORS:
-                text = PLAYER_WINS;
             }
-        switch (this.playerSelection = SCISSORS) {
-            case this.computerSelection = ROCK:
-                text = COMPUTER_WINS;
-                break;
-            case this.computerSelection = SCISSORS:
-                text = TIE;
-                break;
-            case this.computerSelection = PAPER:
-                text = PLAYER_WINS;
-            }
-        switch (this.playerSelection = PAPER) {
-            case this.computerSelection = ROCK:
-                text = PLAYER_WINS;
-                break;
-            case this.computerSelection = SCISSORS:
-                text = COMPUTER_WINS;
-                break;
-            case this.computerSelection = PAPER:
-                text = TIE;
-        }
         // Use this.playerSelection and this.computerSelection to return the appropriate outcome from the Outcomes enumerator above.
     }
 
